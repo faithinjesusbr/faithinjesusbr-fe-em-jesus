cf5a03dedc319f5cef76e4199d3e4aea571383c3
faithinjesusbr <faithinjesuseua@gmail.com>
2025-08-10 13:04:04 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,13 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  darkMode: ["class"],
-  content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
+  content: [
+    './client/index.html',
+    './client/src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: { extend: {} },
+  plugins: []
+} satisfies Config
     extend: {
       borderRadius: {
         lg: "var(--radius)",
