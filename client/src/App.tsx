6ada36479fdfa72e982e9cfc10d542dea68f30d1
faithinjesusbr <faithinjesuseua@gmail.com>
2025-08-10 14:15:35 -0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { Tooltip } from "./components/ui/tooltip";
 import { AuthProvider, useAuth } from "@/hooks/use-auth";
 import NotFound from "@/pages/not-found";
 import Login from "@/pages/login";
