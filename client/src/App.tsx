fdea2f7840aecea6a464416fef557c301c1f79e9
faithinjesusbr <faithinjesuseua@gmail.com>
2025-08-11 09:46:10 -0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -139,12 +139,10 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
         <AuthProvider>
           <Toaster />
           <Router />
         </AuthProvider>
-      </TooltipProvider>
     </QueryClientProvider>
   );
 }
