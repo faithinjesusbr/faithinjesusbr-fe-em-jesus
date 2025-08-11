a3ba7adf952c040125a27d166f7adcee97113a59
faithinjesusbr <faithinjesuseua@gmail.com>
2025-08-11 07:26:21 -0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,11 @@
+import { TooltipProvider } from "@/components/ui/tooltip"
 import React from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import "./index.css";
 
-// se o arquivo estiver em client/src/components/ui/tooltip.tsx use este import:
 import { TooltipProvider } from "@/components/ui/tooltip";
-// se você colocou o arquivo em client/src/components/tooltip.tsx, troque a linha acima por:
-// import { TooltipProvider } from "@/components/tooltip";
-
 import { Toaster } from "@/components/ui/toaster";
 
 createRoot(document.getElementById("root")!).render(
